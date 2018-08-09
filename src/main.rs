9cd3d638306695c45f4ed747d168961e342f676d
Shinichi Kogai <kogai0121@gmail.com>
2018-08-09 21:20:59 +0900

    use repl

--- src/main.rs
+++ src/main.rs
@@ -21,14 +21,23 @@ enum E5 {
     B,
 }
 
+#[repr(C)]
+union E6 {
+    a: u8,
+    b: u16,
+}
+
 fn main() {
     println!("Size is {}", std::mem::size_of::<E1>());
     println!("Size is {}", std::mem::size_of::<E2>());
     println!("Size is {}", std::mem::size_of::<E3>());
     println!("Size is {}", std::mem::size_of::<E4>());
     println!("Size is {}", std::mem::size_of::<E5>());
+    println!("Size is {}", std::mem::size_of::<E6>());
     // Size is 1
     // Size is 4
     // Size is 2
     // Size is 4
+    // Size is 1
+    // Size is 2
 }
